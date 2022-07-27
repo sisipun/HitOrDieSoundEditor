1d03d89c665ec40b1998431edeb25cde2e32b33f
Alexey <sisipun@gmail.com>
2022-07-27 20:29:36 +0300

    Load audio file

--- soundeditor.cpp
+++ soundeditor.cpp
@@ -1,10 +1,18 @@
 #include "soundeditor.h"
 #include "./ui_soundeditor.h"
 
+#include <QFileDialog>
+#include <QSoundEffect>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+
 SoundEditor::SoundEditor(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::SoundEditor)
 {
+    mediaPlayer = new QMediaPlayer(this);
+    audioOutput = new QAudioOutput(this);
+    mediaPlayer->setAudioOutput(audioOutput);
     ui->setupUi(this);
 }
 
@@ -18,5 +26,18 @@ void SoundEditor::on_timeline_valueChanged(int value)
     int maxSeconds = QTime(0, 0, 0).secsTo(ui->timer->maximumTime());
     int seconds = (value / (100.0f - 1.0f)) * maxSeconds;
     ui->timer->setTime(QTime::fromMSecsSinceStartOfDay(seconds * 1000));
+}
+
+
+void SoundEditor::on_loadsound_clicked()
+{
+    QString soundFileName = QFileDialog::getOpenFileName(this, tr("Open sound"), QString(), tr("Sound Files (*.wav)"));
+    if (!soundFileName.isEmpty())
+    {
+        this->soundFileName = soundFileName;
+        ui->soundfilename->setText(soundFileName);
+        mediaPlayer->setSource(QUrl::fromLocalFile(this->soundFileName));
+        mediaPlayer->play();
+    }
 }
 
--- soundeditor.h
+++ soundeditor.h
@@ -5,6 +5,8 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class SoundEditor; }
+class QMediaPlayer;
+class QAudioOutput;
 QT_END_NAMESPACE
 
 class SoundEditor : public QMainWindow
@@ -18,7 +20,12 @@ public:
 private slots:
     void on_timeline_valueChanged(int value);
 
+    void on_loadsound_clicked();
+
 private:
     Ui::SoundEditor *ui;
+    QString soundFileName;
+    QMediaPlayer *mediaPlayer;
+    QAudioOutput *audioOutput;
 };
 #endif // SOUNDEDITOR_H

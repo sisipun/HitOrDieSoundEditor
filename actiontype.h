11cfd095ac3a56b981f366deea8eb77675d60b53
sisipun <sisipun@gmail.com>
2023-03-29 20:17:32 +0200

    Remove reload action type

--- actiontype.h
+++ actiontype.h
@@ -6,19 +6,16 @@
 
 enum class ActionType {
     FIRE,
-    GRENADE,
-    RELOAD
+    GRENADE
 };
 
 const QMap<ActionType, QString> ACTION_TYPE_TO_STRING({
                                                           std::make_pair(ActionType::FIRE, "FIRE"),
-                                                          std::make_pair(ActionType::GRENADE, "GRENADE"),
-                                                          std::make_pair(ActionType::RELOAD, "RELOAD")
+                                                          std::make_pair(ActionType::GRENADE, "GRENADE")
                                                       });
 const QMap<QString, ActionType> STRING_TO_ACTION_TYPE({
                                                           std::make_pair("FIRE", ActionType::FIRE),
-                                                          std::make_pair("GRENATE", ActionType::GRENADE),
-                                                          std::make_pair("RELOAD", ActionType::RELOAD)
+                                                          std::make_pair("GRENATE", ActionType::GRENADE)
                                                       });
 
 #endif // ACTIONTYPE_H
